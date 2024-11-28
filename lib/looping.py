ef9452eadcf7556da15a1a90c206516c6eaac51c
odera <eugeneodera59@gmail.com>
2024-11-28 13:54:05 +0300

    add text

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+   count = 10
+   while count > 0:
+    print(count)
+    count -= 1
+    print("Happy New Year!")
     pass
 
-def square_integers(int_list):
+def square_integers(nums):
+    return [num ** 2 for num in nums]
+ 
+nums = [1, 2, 3, 4, 5]
+squared_elements = square_integers(nums)
+print(squared_elements)
+    
     # code goes here!
-    pass
 
 def fizzbuzz():
+    for i in range(1, 101):
+       if i % 3 == 0 and i % 5 == 0 :
+          print("FizzBuzz")
+       elif i % 3 == 0 :
+          print("Fizz")
+       elif i % 5 == 0 :
+          print("Buzz")
+       else:
+          print(i)
+       
     # code goes here!
-    pass
